85a1c3434d17cc31ac0fe2425ed2c1aeed85c4c7
Denis Rossati Ramos <denis.rossatiramos@gmail.com>
2021-12-15 23:27:15 -0300

    requisito 5

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,5 +1,6 @@
 import csv
 import json
+import xml.etree.ElementTree as ET
 from inventory_report.reports.complete_report import CompleteReport
 from inventory_report.reports.simple_report import SimpleReport
 
@@ -13,10 +14,23 @@ class Inventory:
                 reader = csv.DictReader(file, delimiter=",", quotechar='"')
             if path.endswith('.json'):
                 reader = json.load(file)
+            if path.endswith('.xml'):
+                tree = ET.parse(file)
+                reader = tree.getroot()
             for row in reader:
-                dict_list.append(row)
+                is_xml = path.endswith('.xml') 
+                if (is_xml):
+                    xml_dict = {}
+                    for key in row:
+                        xml_dict[key.tag] = key.text
+                    dict_list.append(xml_dict)
+                else:
+                    dict_list.append(row)
+
 
         if report_type == "simples":
             return SimpleReport.generate(dict_list)
         else:
             return CompleteReport.generate(dict_list)
+
+# solução para leitura do xml retirada de: https://stackabuse.com/reading-and-writing-xml-files-in-python/
