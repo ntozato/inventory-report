240a25db32ec96669078ac84cb6993b603dfb2c2
Denis Rossati Ramos <denis.rossatiramos@gmail.com>
2021-12-16 00:16:49 -0300

    refatorando requisito 5

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -6,31 +6,48 @@ from inventory_report.reports.simple_report import SimpleReport
 
 
 class Inventory:
-    def import_data(path, report_type):
+    @classmethod
+    def process_csv(cls, file):
         dict_list = []
+        reader = csv.DictReader(file, delimiter=",", quotechar='"')
+        for row in reader:
+            dict_list.append(row)
+        return dict_list
 
+    @classmethod
+    def process_json(cls, file):
+        dict_list = []
+        reader = json.load(file)
+        for row in reader:
+            dict_list.append(row)
+        return dict_list
+
+    @classmethod
+    def process_xml(cls, file):
+        dict_list = []
+        document = ET.parse(file)
+        root = document.getroot()
+        for record in root:
+            tag_dict = {}
+            for line in record:
+                tag_dict[line.tag] = line.text
+            dict_list.append(tag_dict)
+        return dict_list
+
+    def import_data(path, report_type):
         with open(path) as file:
+            dict_list = None
             if path.endswith('.csv'):
-                reader = csv.DictReader(file, delimiter=",", quotechar='"')
+                dict_list = Inventory.process_csv(file)
             if path.endswith('.json'):
-                reader = json.load(file)
+                dict_list = Inventory.process_json(file)
             if path.endswith('.xml'):
-                tree = ET.parse(file)
-                reader = tree.getroot()
-            for row in reader:
-                is_xml = path.endswith('.xml') 
-                if (is_xml):
-                    xml_dict = {}
-                    for key in row:
-                        xml_dict[key.tag] = key.text
-                    dict_list.append(xml_dict)
-                else:
-                    dict_list.append(row)
-
+                dict_list = Inventory.process_xml(file)
 
         if report_type == "simples":
             return SimpleReport.generate(dict_list)
         else:
             return CompleteReport.generate(dict_list)
 
-# solução para leitura do xml retirada de: https://stackabuse.com/reading-and-writing-xml-files-in-python/
+# solução para leitura do xml retirada de:
+# https://stackabuse.com/reading-and-writing-xml-files-in-python/
