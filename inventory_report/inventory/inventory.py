4725a50b82b8b1b79522ca350d7e1649286123b6
Nícolas Tozato <ntozato9@gmail.com>
2021-12-17 09:12:38 -0300

    teste

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,18 +1,39 @@
 import csv
+import json
 from inventory_report.reports.complete_report import CompleteReport
 from inventory_report.reports.simple_report import SimpleReport
+import xml.etree.ElementTree as ET
 
 
 class Inventory:
-    def import_data(path, report_type):
-        dict_list = []
-
+    def csv_reader(path):
         with open(path) as file:
             reader = csv.DictReader(file, delimiter=",", quotechar='"')
+            data = []
             for row in reader:
-                dict_list.append(row)
+                data.append(row)
+
+        return data
+    
+    def json_reader(path):
+        with open(path) as file:
+            return json.load(file)
+
+    @classmethod
+    def import_data(cls, path, report_type):
+        dict_list = []
+        file_type = path.split(".")[1]
+
+        if file_type == 'csv':
+            dict_list = cls.csv_reader(path)
+        elif file_type == 'json':
+            dict_list = cls.json_reader(path)
 
         if report_type == "simples":
             return SimpleReport.generate(dict_list)
         else:
             return CompleteReport.generate(dict_list)
+
+        
+
+print(Inventory.import_data('inventory_report/data/inventory.json', 'completo'))
